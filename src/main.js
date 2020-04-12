29c8f47a7e33d209efdc5e03aae4865dd126f7e4
fourierrr <fourierrr@gmai.com>
2020-04-12 23:01:45 +0800

    finish login

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+import './assets/css/global.css'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1'
+Vue.prototype.$http = axios
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 
